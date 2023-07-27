e98c94f604ca5b303370c0d4597158f85e025bcc
Abdulaziz-MI <am.alzaid@outlook.com>
2023-07-27 15:29:51 +0100

    Created Tables

--- safari.sql
+++ safari.sql
@@ -1,4 +1,35 @@
-DROP TABLE IF EXISTS staff;
-DROP TABLE IF EXISTS enclourses;
-DROP TABLE IF EXISTS animals;
-DROP TABLE IF EXISTS assignments;
+DROP TABLE assignments;
+DROP TABLE animals;
+DROP TABLE staff;
+DROP TABLE enclosures;
+
+
+CREATE TABLE enclosures(
+id SERIAL PRIMARY KEY,
+name  VARCHAR(255),
+capacity INT,
+closedForMaintenance BOOLEAN
+);
+
+CREATE TABLE staff(
+id SERIAL PRIMARY KEY,
+name  VARCHAR(255),
+employeeNumber INT
+);
+
+CREATE TABLE animals(
+id SERIAL PRIMARY KEY,
+name  VARCHAR(255),
+type VARCHAR(255),
+age INT,
+enclosure_id INT REFERENCES enclosures(id)
+);
+
+CREATE TABLE assignments(
+id SERIAL PRIMARY KEY,
+employee_id  INT REFERENCES staff(id),
+enclosure_id INT REFERENCES enclosures(id),
+day VARCHAR(255)
+);
+
+
